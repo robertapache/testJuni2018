7d7a5cca28e2306ac1b0921c7898ff76b612bf4d
admin <admin@admins-mbp.fritz.box>
2018-06-23 16:02:29 +0200

    Erster Comic

--- testJuni2018/ViewController.swift
+++ testJuni2018/ViewController.swift
@@ -19,7 +19,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
+    @IBOutlet weak var halloweltbutton: UIButton!
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBAction func buttonclicked(_ sender: Any) {
+    
+        
+        let alert = UIAlertController(title: "Mamma oder Papps", message: "Bist du der Pappa?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ja", style: .default, handler: {action in
+            self.label1.text = "Robert"
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Nein", style: .cancel, handler: { action in
+            self.label1.text = "Inta"
+            
+        }))
+        
+        self.present(alert, animated: true)    }
 }
 
